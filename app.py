6adfc1eebfd893e02e0735e4ac8791ee42101941
Justas Zieminykas <justaxsz@gmail.com>
2024-11-21 09:52:43 +0200

    Final project set up steps, adding basic templates and stylesheet and rendering static contacts data

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, flash
 
 app = Flask(__name__)
+app.secret_key = '*T2<3>g;=E1Kc+N;^GP='
 
 @app.route('/')
 def home():
-    return 'Welcome to the contact list!'
+    contacts = [
+    {
+        'id' :'123213',
+        'full_name': 'John C. Addams',
+    },
+    {
+        'id' :'551232',
+        'full_name': 'Kathleen Ann Williams',
+    }]
+    return render_template('contact_list.html', contacts=contacts)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5008)
